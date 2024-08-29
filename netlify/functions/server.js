42cfc2a13e6d0b05ad84b483345471451ac0d31e
Shubham Kumar <shubhamkr6758@gmail.com>
2024-08-29 22:09:23 +0530

    chore: Update static file path in server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render('index');
