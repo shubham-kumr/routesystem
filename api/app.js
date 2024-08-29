38bdbffb97a4d0c4f47e881a57e2a9e0fe5ee91e
Shubham Kumar <shubhamkr6758@gmail.com>
2024-08-29 22:29:48 +0530

    chore: Update app.js and package.json, remove unnecessary files, update file paths, and update static file path in server.js

--- api/app.js
+++ api/app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const path = require('path');
-const socketio = require('socket.io');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+
+// Set up server
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
+// Route to render index.ejs
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// WebSocket connection handling
 io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
+
     socket.on('send-location', (coords) => {
         io.emit('receive-location', { id: socket.id, ...coords });
     });
@@ -21,9 +34,6 @@ io.on('connection', (socket) => {
     });
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-
-module.exports = app;
+module.exports = (req, res) => {
+    app(req, res);
+};
